6652ca4d02ff19a5acd869b4f498f39df5faa2cb
Robert Thao <robert.lee.thao@gmail.com>
2024-04-19 11:23:56 -0500

    nav options change content on page now

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,22 +1,36 @@
+const navOptions = [
+    {
+        navId: 'nav-find-a-song',
+        pageId: 'find-a-song-container'
+    },
+    {
+        navId: 'nav-add-a-song',
+        pageId: 'add-a-song-container'
+    },
+    {
+        navId: 'nav-about',
+        pageId: 'about-container'
+    },
+    {
+        navId: 'nav-contact-us',
+        pageId: 'contact-us-container'
+    }
+]
 
-const showHomePage = true;
-const showFindASongPage = document.getElementById('find-a-song-container').computedStyleMap.
-const showAddASongPage = false;
-const showAboutPage = false;
-const showContactUsPage = false;
-
-function pageNavigation() {
-
-}
-
-function showSearchFilters(){
-    let currentStatus = document.getElementById("search-filters");
-    console.log(currentStatus.style);
+function showPage(id) {
+    navOptions.forEach(option => {
+        if(option.navId != id){
+            document.getElementById(option.pageId).style.visibility = "hidden";
+            return;
+        }else {
+            document.getElementById(option.pageId).style.visibility = "visible";
+            return;
+        }
+    })
 }
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    console.log(showFindASongPage);
-// document.getElementById('search-filter-visibility').onclick = () => {showSearchFilters()};
+    
 
 }) 
