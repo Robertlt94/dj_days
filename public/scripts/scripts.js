82c165d7640f0275ef5062a70a2cd7ddb55c9734
Robert Thao <robert.lee.thao@gmail.com>
2024-04-20 03:39:06 -0500

    layout changes and json server representation added. Content not showing - still troubleshooting

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -17,6 +17,13 @@ const navOptions = [
     }
 ]
 
+const songsAvailable = document.querySelector('#music-cards');
+const currentSongInfo = document.getElementById('info-banner');
+const currentSongImage = document.getElementById('now-playing-visual');
+const form = document.getElementById('song-form');
+// until file upload is figured out
+const defaultImage = "./images/at-least";
+
 function showPage(id) {
     navOptions.forEach(option => {
         if(option.navId != id){
@@ -25,12 +32,75 @@ function showPage(id) {
         }else {
             document.getElementById(option.pageId).style.visibility = "visible";
             return;
-        }
-    })
-}
+        };
+    });
+};
+
+function fetchMusicLibrary() {
+    fetch('/musicLibrary')
+        .then(response => response.json())
+        .then(data => {
+            songsAvailable.innerHTML = '';
+            data.forEach((song, index) => {
+                const li = document.createElement('li');
+                li.innerHTML = 
+                    `<div class="album-container">
+                        <img src="${song.image}" alt="${song.artist}">
+                        <div class="album-content">
+                            <h3>${song.title}(${song.released})</h3>
+                            <p>${song.artist}</p>
+                            <a href="#" class="album-select-btn">
+                                More
+                            </a>
+                        </div>
+                    </div>`
+                li.onclick = () => updateNowPlaying(index);
+                songsAvailable.appendChild(li);
+            });
+        })
+        .catch(error => console.error('Error fetching songs from music library: ', error));
+};
+
+function updateNowPlaying(index) {
+    fetch('/musicLibrary')
+        .then(response => response.json())
+        .then(musicLibrary => {
+            const song = musicLibrary[index];
+            currentSongInfo.innerHTML = 
+                ``
+            currentSongImage.innerHTML = 
+                `<img src="${song.image}" alt="${song.artist}">
+                <audio controls autoplay>
+                    <source src="${song.url}" type="audio/mp3">
+                </audio>`
+        })
+        .catch(error => console.error('Error updating now playing section: ', error));
+};
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    
+    form.addEventListener('submit', function(event) {
+        const newSong = {
+            title: document.getElementById('title-input').value,
+            artist: document.getElementById('artist-input').value,
+            released: document.getElementById('release-date-input').value,
+            url: document.getElementById('url-input').value,
+            image: defaultImage
+        };
+        fetch('/musicLibrary', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newSong)
+        })
+        .then(() => {
+            fetchMusicLibrary();
+            form.reset();
+            confirm("Song Added!")
+        })
+        .catch(error => console.error("Error posting new song: ", error));
+    })
 
+    fetchMusicLibrary();
 }) 
--- server.js
+++ server.js
@@ -1,13 +1,44 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
+let musicLibrary = [];
+
+function loadMusicLibrary() {
+    fs.readFile(path.join(_dirname, "data", "musicLibrary.json"), 'utf8', (err, data) => {
+        if(err){
+            console.log("error reading library: ", err);
+            return;
+        };
+        musicLibrary = JSON.parse(data);
+    });
+}
+
+app.get('/musicLibrary', (req, res) => {
+    res.json(musicLibrary);
+});
+
+app.post('/musicLibrary', (req, res) => {
+    const newSong = req.body;
+    musicLibrary.push(newSong);
+
+    fs.writeFile(path.join(_dirname, "data", "musicLibrary.json", JSON.stringify(musicLibrary, null, 2), 'utf8', (err) => {
+        if(err){
+            res.status(500).json({error: "Error writing the song into the JSON"});
+            return;
+        }
+        res.status(201).json({message: 'Song written into JSON successfully'})
+    }));
+});
+
 app.listen(PORT, () => {
-    console.log(`Server is up on PORT: ${PORT}`);
+    console.log(`Server is up on https://localhost:${PORT}`);
 })
